87e6ab10c16e8f2a5d9ba8199b5a3640c12b4ee3
ldidone <lucas_crar14@hotmail.com>
2023-07-16 20:20:58 -0300

    Updated pdf2jpg module.

--- pdf2jpg.py
+++ pdf2jpg.py
@@ -5,22 +5,20 @@ from pdf2image import convert_from_path
 
 
 class Pdf2Jpg:
-    def __init__(self, base_input_path, base_output_path) -> None:
-        self.base_input_path = base_input_path
+    def __init__(self, base_output_path=None) -> None:
         self.base_output_path = base_output_path
-        self.file_paths = self.__get_file_paths()
 
-    def __get_file_paths(self):
-        file_paths = os.listdir(self.base_input_path)
-        file_paths = [
-            f for f in file_paths if os.path.isfile(self.base_input_path + "/" + f)
-        ]
+    def get_file_paths(self, input_path):
+        file_paths = os.listdir(input_path)
+        file_paths = [f for f in file_paths if os.path.isfile(input_path + "/" + f)]
+        return file_paths
 
-    def convert_files(self):
-        q_files = len(self.file_paths)
-        for i, file_path in enumerate(self.file_paths):
-            file_path_full = f"{base_input_path}/{file_path}"
-            print(f"Converting file {file_path_full} | [{i}/{q_files}]")
+    def convert_files(self, input_path):
+        file_paths = self.get_file_paths(input_path)
+        q_files = len(file_paths)
+        for i, file_path in enumerate(file_paths):
+            file_path_full = f"{input_path}/{file_path}"
+            print(f"Converting file {file_path_full} | [{i+1}/{q_files}]")
             images = convert_from_path(file_path_full)
             cv2_images = []
             for image in images:
@@ -29,7 +27,7 @@ class Pdf2Jpg:
                 cv2_images.append(img)
             image = cv2.vconcat(cv2_images)
             file_name = file_path.split(".")[0]
-            output_path = f"{base_output_path}/{file_name}.jpg"
+            output_path = f"{self.base_output_path}/{file_name}.jpg"
             cv2.imwrite(output_path, image)
         print("All files were converted successfully!")
 
@@ -38,5 +36,5 @@ if __name__ == "__main__":
     base_input_path = "data/inputs/invoices/pdfs"
     base_output_path = "data/inputs/invoices/jpgs"
 
-    converter = Pdf2Jpg(base_input_path, base_output_path)
-    converter.convert_files()
+    converter = Pdf2Jpg(base_output_path)
+    converter.convert_files(base_input_path)
