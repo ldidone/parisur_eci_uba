ceb715a871b465aeebb0967fc50c5725e4e12186
ldidone <lucas_crar14@hotmail.com>
2023-07-18 20:21:59 -0300

    Exctract info from images.

--- script.py
+++ script.py
@@ -3,12 +3,8 @@ import easyocr
 import pandas as pd
 import numpy as np
 from pdf2jpg import Pdf2Jpg
+import time
 
-
-class InvoiceData:
-     def __init__(self, invoice_number, total_charged) -> None:
-          self.invoice_number=invoice_number
-          self.total_charged=total_charged
 
 class OCRInputer:
      def __init__(self, input_path, output_path, csv_path, use_gpu=False) -> None:
@@ -19,45 +15,62 @@ class OCRInputer:
           self.reader = easyocr.Reader(["es", "en"], gpu=use_gpu)
           self.df = self.__read_csv_data(csv_path)
           self.invoice_numbers = list(self.df.invoice_number)
+          self.total_strings = ['Total:']
+          self.error_totals = []
 
-     def extract_info(self, input_path, output_path, verbose=False):
+     def extract_info(self, input_path):
           image = cv2.imread(input_path)
           result = self.reader.readtext(image, paragraph=False)
 
           invoice_number = None
           total_charged = np.NaN
           for res in result:
-               if verbose:
-                    print("res:", res)
+               if not invoice_number:
+                    for invoice in self.invoice_numbers:
+                         if invoice.upper() in res[1].upper():
+                              invoice_number = invoice
+                              print(f'Invoice number found: {invoice_number}')
+               else:
+                    for total in self.total_strings:
+                         if total.upper() in res[1].upper():
+                              try:
+                                   total_charged = res[1].split(': ')[1].replace('$', '')
+                                   total_charged = float(total_charged.replace(',', ''))
+                                   print(f'Total charged found: {total_charged}')
+                              except:
+                                   print(f'Error ocurred with the total of invoice_number: {invoice_number}')
+                                   self.error_totals.append((invoice_number, res[1]))
+                                   continue
+          
+          if invoice_number:
+               self.df.loc[self.df.invoice_number == invoice_number, 'total_charged'] = total_charged
+     
+     def export_results(self):
+          print('Saving results...')
+          self.df.rename(columns={'invoice_number': 'Invoice Number',
+                                   'total_charged': 'Total Charged'}, inplace=True)
+          full_output_path = f'{self.base_output_path}invoice_numbers_{round(time.time())}.csv'
+          self.df.to_csv(full_output_path, index=False)
 
-               pt0 = res[0][0]
-               pt1 = res[0][1]
-               pt2 = res[0][2]
-               pt3 = res[0][3]
-               pt0 = [round(pto) for pto in pt0]
-               pt1 = [round(pto) for pto in pt1]
-               pt2 = [round(pto) for pto in pt2]
-               pt3 = [round(pto) for pto in pt3]
-               cv2.rectangle(image, pt0, (pt1[0], pt1[1] - 23), (166, 56, 242), -1)
-               cv2.putText(image, res[1], (pt0[0], pt0[1] - 3), 2, 0.8, (255, 255, 255), 1)
-               cv2.rectangle(image, pt0, pt2, (166, 56, 242), 2)
+     def exctract_all_inputs(self, n=None):
 
-               """if res[1] in self.invoice_numbers:
-                    invoice_number = res[1]
-               elif res[1].split(':')[1].replace(' ', '') in self.invoice_numbers:
-                    invoice_number = res[1].split(':')[1].replace(' ', '')"""
-                
-          cv2.imwrite(output_path, image)
-          if invoice_number is not None:
-               print(f'INVOICE NUMBER: {invoice_number}')
-
-     def exctract_all_inputs(self, n):
-          q_files = len(self.file_paths[:n])
-          for i, file_path in enumerate(self.file_paths[:n]):
+          q_files = len(self.file_paths[:n]) if n else len(self.file_paths)
+          for i, file_path in enumerate(self.file_paths[:q_files]):
                input_path = f"{self.base_input_path}/{file_path}"
-               output_path = f"{self.base_output_path}/{file_path}"
                print(f"Exctracting info from file {file_path} | [{i+1}/{q_files}]")
-               self.extract_info(input_path, output_path, verbose=False)
+               self.extract_info(input_path)
+          
+          q_found = self.df[~self.df['total_charged'].isna()].shape[0]
+
+          print('*' * 50)
+          print(f'Found {q_found}/{q_files}')
+          print('*' * 50)
+
+          self.export_results()
+
+          if len(self.error_totals) > 0:
+               print('************* Errors *************')
+               print(self.error_totals)
      
      def __read_csv_data(self, csv_path):
           df = pd.read_csv(csv_path)
@@ -68,7 +81,7 @@ class OCRInputer:
 
 if __name__ == "__main__":
     base_input_path = "data/inputs/invoices/jpgs"
-    base_output_path = "data/outputs/jpgs"
+    base_output_path = "data/outputs/"
     csv_path = "data/inputs/2023-06-26-invoice_numbers-final.csv"
 
     inputer = OCRInputer(base_input_path, base_output_path, csv_path)
